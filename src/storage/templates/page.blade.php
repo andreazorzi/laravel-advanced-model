be59ace98814c487c4e50f6dc3b13b638366529f
Andrea Zorzi <info@zorziandrea.com>
2026-07-14 17:22:07 +0200

    Fix page generations

--- src/storage/templates/page.blade.php
+++ src/storage/templates/page.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
     {{-- Head --}}
-    <x-backoffice.head :title="__('advanced-model::modal.title', ["model" => ":MODEL_NAME:" ])" />
+    <x-backoffice.head :title="__('advanced-model::modal.title', ['model' => ':MODEL_NAME:'])" />
     
     <body class="container-fluid" hx-headers='{"X-CSRF-TOKEN": "{{csrf_token()}}"}' hx-ext="ajax-header" hx-disabled-elt="this" hx-target="#request-response">
         {{-- Header --}}
         <x-backoffice.header />
 
         <div id="container">
-            <x-backoffice.title :title="__('advanced-model::modal.title', ["model" => ":MODEL_NAME:" ]) subtitle="Aggiungi e modifica :MODEL_NAME_LOWER:" />
+            <x-backoffice.title :title="__('advanced-model::modal.title', ['model' => ':MODEL_NAME:'])"" subtitle="Aggiungi e modifica :MODEL_NAME_LOWER:" />
         
             {{-- Search Table --}}
             <x-search-table::table :model="new App\Models\:MODEL_NAME:()" showadvancefilters="offcanvas" />
